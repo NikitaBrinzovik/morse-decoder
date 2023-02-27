8739349ffe69f8c593367d3e3905b597b67907ec
Nikita Kuznetsov <75593249+NikitaBrinzovik@users.noreply.github.com>
2023-02-27 21:31:35 +0300

    дд

--- src/index.js
+++ src/index.js
@@ -1,44 +1,75 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
-    // write your solution here
+    const peaces = [];
+    let part = '';
+    let slice = '';
+    let result = '';
+    let peaceNumber = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        if (i + 10 > expr.length) {
+            part = expr.slice(i);
+            slice = part.padStart(10, '0');
+            part.push(slice);
+        } else {
+            peaces.push(expr.slice(i, i + 10));
+        }
+    }
+
+    peaces.forEach((peace) => {
+        let res = '';
+        for (let i = 0; i < peace.length; i += 2) {
+            peaceNumber = peace.slice(i, i + 2);
+            if (peaceNumber === "11") res += "-";
+            if (peaceNumber === "10") res += ".";
+            if (peaceNumber.includes("*")) {
+                res += " ";
+                break;
+            }
+        }
+
+        result += res.includes(" ") ? ' ' : MORSE_TABLE[res];
+    });
+
+    return result;
 }
 
 module.exports = {
